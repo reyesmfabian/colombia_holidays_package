e806b640670eb0cbd11bce26ff55985e3c69117b
Fabian Reyes <fabian.reyes@pragma.com.co>
2022-07-14 16:53:24 -0500

    add documentation

--- lib/services/holidays_service.dart
+++ lib/services/holidays_service.dart
@@ -7,6 +7,13 @@ import 'package:flutter/services.dart';
 class ColombiaHolidays {
   final path = 'packages/colombia_holidays/assets/holidays.json';
 
+  ///
+
+  /// **Esta función devuelve una lista de días festivos para un año determinado**
+  ///
+  /// Args:
+  ///   * [year]: El año para el que desea obtener los festivos..
+  ///
   Future<List<Holiday>> getHolidays({required int year}) async {
     year < 1900 ? throw ArgumentError('Year must be greater than 1900') : null;
     final response = await rootBundle.loadString(path);
@@ -15,15 +22,25 @@ class ColombiaHolidays {
     return result.holidays;
   }
 
+  /// **Comprueba si la fecha dada es un día festivo.**
+  ///
+  /// Args:
+  ///   * [day]: Día del mes.
+  ///   * [month]: Mes del año.
+  ///   * [year]: Mes del año.
+  ///
+
   Future<bool> isHoliday(
       {required int day, required int month, required int year}) async {
     month < 0 || month > 12
         ? throw ArgumentError('Month must be between 0 and 12')
         : null;
-    day < 0 || day > 31
-        ? throw ArgumentError('Day must be between 1 and 31')
-        : null;
-    year < 1900 ? throw ArgumentError('Year must be greater than 1900') : null;
+    if (day < 0 || day > 31) {
+      throw ArgumentError('Day must be between 1 and 31');
+    }
+    if (year < 1900 || year > 2299) {
+      throw ArgumentError('Year must be between 1900 and 2299');
+    }
 
     bool isHoliday = false;
     String finalMoth = month < 10 ? '0$month' : '$month';
